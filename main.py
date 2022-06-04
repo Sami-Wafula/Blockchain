c165179211b852b9053b3edf1c7522bd9d058013
Sami <samiwafula3257@gmail.com>
2022-06-04 22:04:54 +0300

    Edit main.py Added the first blockchain

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import datetime
 import json
 import hashlib
 from flask import Flask, jsonify
+
+class Blockchain:
+	def __init__(self):
+		self.chain = []
+		self.create_blockchain(proof=1, previous_hash='0')
